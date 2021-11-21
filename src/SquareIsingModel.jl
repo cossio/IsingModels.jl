c1e53a005653e3eca32172a359aed7f270daab0f
cossio <j.cossio.diaz@gmail.com>
2021-11-21 13:06:09 +0100

    onsager internal energy and heat capacity

--- src/SquareIsingModel.jl
+++ src/SquareIsingModel.jl
@@ -1,5 +1,6 @@
 module SquareIsingModel
     using Statistics, LinearAlgebra
+    using SpecialFunctions: ellipk, ellipe
 
     include("onsager.jl")
     include("ising.jl")
--- src/onsager.jl
+++ src/onsager.jl
@@ -16,3 +16,26 @@ function onsager_magnetization(β::Real)
     @assert β ≥ 0
     return max(1 - csch(2β)^4, 0)^(1/oftype(β, 8))
 end
+
+"""
+    onsager_internal_energy(β)
+
+Internal energy per site of the two-dimensional Ising model, derived by
+Onsager in the thermodynamic limit.
+"""
+function onsager_internal_energy(β::Real)
+    k = 2sinh(2β) / cosh(2β)^2
+    return -2coth(2β) * (1 + 2/π * (2tanh(2β)^2 - 1) * ellipk(k^2))
+end
+
+"""
+    onsager_heat_capacity(β)
+
+Specific heat capacity of the two-dimensional Ising model, derived by
+Onsager in the thermodynamic limit.
+"""
+function onsager_heat_capacity(β::Real)
+    k = 2sinh(2β) / cosh(2β)^2
+    k_ = 2tanh(2β)^2 - 1
+    β * coth(2β)^2 * 2/π * (2ellipk(k^2) - 2ellipe(k^2) - (1 - k_) * (π/2 + k_ * ellipk(k^2)))
+end
