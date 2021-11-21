a31f783c53f09fed454a72dad6abaf727dcc5f1b
cossio <j.cossio.diaz@gmail.com>
2021-11-21 17:42:51 +0100

    improve numerical accuracy of onsager_heat_capacity

--- src/onsager.jl
+++ src/onsager.jl
@@ -24,7 +24,7 @@ Internal energy per site of the two-dimensional Ising model, derived by
 Onsager in the thermodynamic limit.
 """
 function onsager_internal_energy(β::Real)
-    k = 2sinh(2β) / cosh(2β)^2
+    k = 2tanh(2β) / cosh(2β)
     j = 2tanh(2β)^2 - 1
     K = ellipk(k^2)
     return -coth(2β) * (1 + 2/π * j * K)
@@ -37,11 +37,11 @@ Specific heat capacity of the two-dimensional Ising model, derived by
 Onsager in the thermodynamic limit.
 """
 function onsager_heat_capacity(β::Real)
-    k = 2sinh(2β) / cosh(2β)^2
+    k = 2tanh(2β) / cosh(2β)
     K = ellipk(k^2)
     E = ellipe(k^2)
     j = 2tanh(2β)^2 - 1
-    β^2 * coth(2β)^2 * (2/π) * (2K - 2E - (1 - j) * (π/2 + j * K))
+    return β^2 * coth(2β)^2 * (2/π) * (((j - 1//2)^2 + 7//4) * K - 2E - (1 - j) * π / 2)
 end
 
 """
