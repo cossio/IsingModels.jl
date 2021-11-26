861cb59452bb6c8dead4cb9bbdd31d53a18dc36b
cossio <j.cossio.diaz@gmail.com>
2021-11-26 21:23:44 +0100

    heat capacity duality test

--- test/onsager.jl
+++ test/onsager.jl
@@ -24,12 +24,23 @@ end
     for T in 0.5:0.5:5
         @test u'(T) ≈ Ising.onsager_heat_capacity(1/T) rtol=0.0001
     end
+    for β = 0.1:0.1:1
+        @test -β^2 * Ising.onsager_internal_energy'(β) ≈ Ising.onsager_heat_capacity(β)
+    end
 end
 
-@testset "Kramers-Wannier duality of heat capacity and internal energy" begin
+@testset "Kramers-Wannier duality of internal energy and heat capacity" begin
     for β in 0.1:0.1:1
-        Ehere = Ising.onsager_internal_energy(β)
-        Edual = Ising.onsager_internal_energy(Ising.kramers_wannier(β))
-        @test Edual ≈ -2cosh(2β) - sinh(2β) * Ehere
+        β_dual = Ising.kramers_wannier(β)
+        E_here = Ising.onsager_internal_energy(β)
+        E_dual = Ising.onsager_internal_energy(β_dual)
+        @test E_dual ≈ -2cosh(2β) - sinh(2β) * E_here
+
+        C_here = Ising.onsager_heat_capacity(β)
+        C_dual = Ising.onsager_heat_capacity(β_dual)
+
+        x_dual = C_dual / β_dual^2
+        x_here = C_here / β^2
+        @test x_dual ≈ (x_here - 4) * sinh(2β)^2 - sinh(4β) * E_here
     end
 end
